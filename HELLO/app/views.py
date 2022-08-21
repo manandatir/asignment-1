107237d430c617be343ed3dfb8f8a4382ba22cac
manandatir <111643714+manandatir@users.noreply.github.com>
2022-08-21 16:23:50 +0530

    Update views.py

--- HELLO/app/views.py
+++ HELLO/app/views.py
@@ -3,10 +3,10 @@ from .models import Student
 from datetime import datetime
 # Create your views here.
 def index(request):
-    # return HttpResponse("Hello this a home page ",  datetime.now())
-# def STUDENT(request):
-    obj=Student.objects.all()
-    context={
-        "obj":obj,
-    }
-    return render(request, "index.html",context)
+     return HttpResponse("Hello this a home page ",  datetime.now())
+
+#     obj=Student.objects.all()
+#     context={
+#         "obj":obj,
+#     }
+#     return render(request, "index.html",context)
